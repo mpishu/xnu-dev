3fa5298696d77e41a1137f3ddba790ac41c8de72
Pishu Mahtani <mpishu@users.noreply.github.com>
2023-11-02 13:25:00 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# xnu-dev
- XNU Kernel Internals
+# XNU Kernel Internals
+
+The following are some macOS programs that use `mach` syscalls to perform various functions.
+
+## Kernel Version
+
+A command line program that prints the kernel version information.
---
+++ kernel-version/kernel-version.c
@@ -0,0 +1,42 @@
+//
+//  main.c
+//  kernel-version
+//
+//  Created by Pishu Mahtani on 2nd November 2023.
+//
+
+#include <stdio.h>
+#include <mach/mach.h>
+
+#define MAX_VERSION_LENGTH 1024
+
+void getKernelVersion(char *version, size_t maxVersionLength) {
+    host_name_port_t host = mach_host_self();
+
+    if (host == MACH_PORT_NULL) {
+        fprintf(stderr, "Failed to get host port\n");
+        return;
+    }
+
+    kern_return_t kr = host_kernel_version(host, version);
+
+    if (kr != KERN_SUCCESS) {
+        fprintf(stderr, "Failed to get kernel version (Error: %d)\n", kr);
+        return;
+    }
+}
+
+int main(void) {
+    char version[MAX_VERSION_LENGTH];
+
+    // Get the kernel version
+    getKernelVersion(version, sizeof(version));
+
+    if (version[0] != '\0') {
+        printf("Kernel Version: %s\n", version);
+    } else {
+        fprintf(stderr, "Kernel version not available\n");
+    }
+
+    return 0;
+}
